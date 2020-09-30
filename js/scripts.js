ba311aa0b2b487f65ba06802c650f3c1a6aa6925
Gabriel Souto <gabriel.souto@ccc.ufcg.edu.br>
2020-09-30 09:54:01 -0300

    fix colapsar do navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-$(document).ready(function () {
-  $("#sidebar").mCustomScrollbar({
-    theme: "minimal",
-  });
-
-  $("#sidebarCollapse").on("click", function () {
-    $("#sidebar, #content").toggleClass("active");
-    $(".collapse.in").toggleClass("in");
-    $("a[aria-expanded=true]").attr("aria-expanded", "false");
-  });
-});
 
 // Função que pega os estados do Brasil na API do IBGE e popula o select
 
