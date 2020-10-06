7f3326f3cd5aeefce1e7e873f0f05cebfd90d5ce
Daniel Barros de Oliveira <danielbarrosdeoliveira@outlook.com>
2020-10-06 00:55:41 -0300

    alterado para listar paises da america do sul

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,50 @@
+// Função que pega os países da américa do sul, e popula a tabela
+async function populateCountries() {
+  const tableOfCountries = document.querySelector("#tableOfCountries tbody");
 
-// Função que pega os estados do Brasil na API do IBGE e popula o select
+  const url =
+    "http://queimadas.dgi.inpe.br/queimadas/dados-abertos/api/focos/count";
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-function populateUFs() {
-  const ufSelect = document.querySelector("select[name=uf]");
+  const response = await fetch(proxyurl + url);
+  const result = await response.json();
 
-  const URL = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
-  const options = {
-    method: "GET",
-    mode: "cors",
-  };
+  keysSorted = Object.keys(result).sort(function (a, b) {
+    return result[b] - result[a];
+  });
 
-  fetch(URL, options)
-    .then((response) => response.json())
-    .then((states) => {
-      for (const state of states) {
-        ufSelect.innerHTML += `<option value=${state.id}>${state.sigla}</option>`;
-      }
-    });
+  let index = 1;
+
+  keysSorted.forEach((key) => {
+    tableOfCountries.innerHTML += `
+    <tr>
+      <td>${index++}</td>
+      <td>${key}</td>
+      <td>${result[key]}</td>
+    </tr>
+    `;
+  });
+
+  let totalFires = 0;
+
+  keysSorted.forEach((key) => {
+    totalFires += result[key];
+  });
+
+  refreshCardTotalFires(totalFires);
 }
 
-populateUFs();
+// Function that refresh card with total fires in Sul America
 
-$('#sidebarCollapse').on('click', function () {
-  $('#sidebar').toggleClass('active');
-  $(this).toggleClass('active');
+function refreshCardTotalFires(totalFires) {
+  const cardTotalFires = document.getElementById("totalFires");
+  cardTotalFires.innerHTML = `
+  ${totalFires}`;
+}
+
+populateCountries();
+
+$("#sidebarCollapse").on("click", function () {
+  $("#sidebar").toggleClass("active");
+  $(this).toggleClass("active");
 });
